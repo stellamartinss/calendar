5c96cc41ad963d413eeefe0db39fa2dd580d6f5c
stellamartinscrp <stella@edujob.comls>
2023-03-08 19:08:35 -0300

    set weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,10 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class WeatherService {
 
+  constructor(private http: HttpClient){}
+
   getWeatherInformation(city: string) {
-    return {
-      city,
-      weatherInfo: 'here',
-    };
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4b5e15297fb7afbce9fe2b0cf7bf5f5f`;
+
+    return this.http.get<any>(url);
   }
 }
